eb21bc499e082fde0cc58647f5ebf63cb9a6b8f5
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-04-15 15:43:55 +0200

    Remove date from trained data

--- rnn.py
+++ rnn.py
@@ -1,5 +1,4 @@
 import sys
-from datetime import datetime
 
 import torch
 from torch import nn, optim, tensor
@@ -13,7 +12,7 @@ import matplotlib.pyplot as plt
 class PredictorModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn = nn.RNN(input_size=7, hidden_size=16, batch_first=True)
+        self.rnn = nn.RNN(input_size=6, hidden_size=16, batch_first=True)
         self.fc = nn.Linear(16, 1)
         self.to(torch.float32)
         self.optimizer = optim.Adam(self.parameters())
@@ -38,14 +37,16 @@ if __name__ == "__main__":
     model = PredictorModel()
     data = pd.read_csv(
         'NVDA.csv',
-        sep=',').to_numpy()[1:]
-    for row in data:
-        row[0] = datetime.strptime(row[0], "%d-%m-%Y").timestamp()
+        sep=','
+    )
 
+    data = data.iloc[:, 1:]  # drop date
+    data = data.to_numpy()
     data = data.astype(np.float32)
 
-    T = torch.from_numpy(data[:-1])
-    Y = torch.from_numpy(data[1:, 5])
+    T = torch.tensor(data[:-1])
+    Y = torch.tensor(data[1:, 4]).unsqueeze(1)
+
     loss_over_epoch = []
     for epoch in range(EPOCHS):
         loss = model.fit(T, Y)
@@ -55,6 +56,5 @@ if __name__ == "__main__":
         sys.stdout.flush()
     sys.stdout.write('\n')
 
-    # print("prediction: " + model(torch.from_numpy(data[-1])))
     plt.plot(np.linspace(0, EPOCHS, EPOCHS), loss_over_epoch)
     plt.show()
