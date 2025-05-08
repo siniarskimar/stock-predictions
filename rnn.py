b636f72fb505afcf32b0f2e0489bf2d38cd0ddcc
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-05-08 14:35:24 +0200

    Skrypt RNN oskara

--- rnn.py
+++ rnn.py
@@ -1,47 +1,65 @@
 import sys
-
 import torch
 from torch import nn, optim, tensor
-
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
+import math
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+minArr = np.full(shape=6, fill_value=10**8, dtype=float)
+maxArr = np.full(shape=6, fill_value=0, dtype=float)
+
+
+def min_max(data: np.array) -> None:
+
+    for row in data:
+        for idx in range(len(row)):
+            if (row[idx] > maxArr[idx]):
+                maxArr[idx] = row[idx]
+            if (row[idx] < minArr[idx]):
+                minArr[idx] = row[idx]
+
+    for idx, sequence in enumerate(data):
+        for i, el in enumerate(sequence):
+            el = (el - minArr[i])/(maxArr[i] - minArr[i])
+            data[idx][i] = el
+
+
+def create_sets(data: np.array, length: int) -> np.array:
+    x, y = [], []
+    for i in range(len(data) - length):
+        x.append(data[i:i + length])
+        y.append(data[i + length])
+    return np.array(x), np.array(y)
 
 
 class PredictorModel(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.rnn = nn.RNN(input_size=6, hidden_size=16, batch_first=True)
-        self.fc = nn.Linear(16, 1)
+        super(PredictorModel, self).__init__()
+        self.rnn = nn.RNN(input_size=6, hidden_size=50, batch_first=True)
+        self.fc = nn.Linear(50, 6)
         self.to(torch.float32)
-        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
-        self.loss = nn.MSELoss()
 
-    def forward(self, xs):
+    def forward(self, xs: tensor) -> tensor:
         out, _ = self.rnn(xs)
-        return self.fc(out)
-
-    def fit(self, ts: tensor, ys: tensor) -> float:
-        self.optimizer.zero_grad()
-        y_pred = self.forward(ts)
-        loss = self.loss(y_pred, ys)
-        loss.backward()
-        self.optimizer.step()
-        return loss.item()
+        # print("_", _)
+        # print("out", out)
+        # print(out.shape)
+        # print("out", out[:, -1, :])
+        # print("out: ", self.fc(out[:, -1, :]))
+        return self.fc(out[:, -1, :])
 
 
-def main():
-    EPOCHS = 5000
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f'Using device: {device.type}')
-
-    if device.index is not None:
-        print(f'Device index: {device.index}')
+if __name__ == "__main__":
+    EPOCHS = 200
 
     model = PredictorModel()
-    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    loss_fn = nn.MSELoss()
+
     data = pd.read_csv(
         'NVDA.csv',
         sep=','
@@ -51,21 +69,47 @@ def main():
     data = data.to_numpy()
     data = data.astype(np.float32)
 
-    T = torch.tensor(data[:-1]).to(device)
-    Y = torch.tensor(data[1:, 4]).unsqueeze(1).to(device)
+    min_max(data)
 
-    loss_over_epoch = []
+    x, y = create_sets(data, 10)
+    # print(x)
+    # exit(420)
+
+    split_index = int(len(data) * 0.85)
+    x_train, x_test = x[:split_index], x[split_index:]
+    y_train, y_test = y[:split_index], y[split_index:]
+
+    x_train = torch.tensor(x_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+    x_test = torch.tensor(x_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
+
     for epoch in range(EPOCHS):
-        loss = model.fit(T, Y)
-        loss_over_epoch.append(loss)
-        sys.stdout.write("\r                                       \r")
-        sys.stdout.write(f"epoch: {epoch} loss: {loss}")
-        sys.stdout.flush()
-    sys.stdout.write('\n')
+        model.train()
 
-    plt.plot(np.linspace(0, EPOCHS, EPOCHS), loss_over_epoch)
+        optimizer.zero_grad()
+        y_pred = model.forward(x_train)
+
+        loss = loss_fn(y_pred, y_train)
+        loss.backward()
+        optimizer.step()
+        # print(loss.item())
+
+        model.eval()
+        with torch.no_grad():
+            test_pred = model(x_test)
+            test_loss = loss_fn(test_pred, y_test)
+            print(f"Test Loss:{test_loss.item()}")
+
+    print(y_test)
+    print(" ")
+    print(test_pred)
+
+    # plt.plot(test_pred[:len(y_test), 2], label="predicted values")
+    # plt.plot(y_test[:, 2], label="true values")
+    # plt.show()
+
+    plt.plot(test_pred, label="predicted")
+    plt.plot(y_test, label="true values")
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
+    plt.show()
